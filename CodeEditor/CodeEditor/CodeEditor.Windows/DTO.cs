4c1983daaf8194e6c572a75462d2fed9c7bed332
Andrii <Pinoplast@users.noreply.github.com>
2016-05-19 11:30:03 +0300

    Compile client

---
+++ CodeEditor/CodeEditor/CodeEditor.Windows/DTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+namespace DTO
+{
+    public class Command
+    {
+        
+        public string CommandText { set; get; }
+        public Color CommandColor { set; get; }
+    }
+}
---
+++ CodeEditor/CodeEditor/CodeEditor.Windows/Refactor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using DTO;
+namespace Refactor
+{
+    public class Refactor
+    {
+        static List<Command> list;
+        public Refactor()
+        {
+            list = new List<Command>();
+            Command c = new Command() { CommandText = "for", CommandColor = Colors.Blue};
+        }
+        public Command Refactoring(string str)
+        {
+            foreach(Command i in list)
+            {
+                if (str == i.CommandText)
+                {
+                    return i;
+                }
+            }
+            return new Command() { CommandText = str, CommandColor = Colors.Black };
+        }
+    }
+}
